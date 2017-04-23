4f4398f49e7e14dabd5edb48d99b1a53dfdce461
M2do <mathura.dakshana15@gmail.com>
2017-04-23 12:39:36 +0530

    Adding algo2016 folder

---
+++ algo2016/asgn12GraphConnectivity.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Graph
+{
+	int V;
+	list <*list> adjList;
+public:
+	Graph();
+	void addEdge(int src,int dst);
+};
+int main()
+{
+	return 0;
+}
