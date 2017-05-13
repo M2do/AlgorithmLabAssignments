f1283587db13156d289deabdc9b1a5ba2ab01166
M2do <mathura.dakshana15@gmail.com>
2017-05-13 10:56:18 +0530

    did assignment 4 a half

---
+++ Algo2015/assignment4/Asgn4.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	/*
+	ifstream file;
+	file.open("input.txt", ios::in);
+    if(!file.is_open())
+    {
+		cout<<"error while opening ";
+    }
+    else
+    {
+		string line;
+		while(file.good())
+		{
+			getline(file,line);
+			cout<<line<<endl;
+		}
+    }
+    */
+    
+	return 0;
+}
